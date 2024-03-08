5527863acb4eca34026444589eda4b905ca9bb63
Isaac Sunday Arinze <sunnyanas6@gmail.com>
2024-03-08 18:53:14 +0100

    Using prepare statement interface to enter into dataabse via the console

--- src/week4/JDBCDemo5.java
+++ src/week4/JDBCDemo5.java
@@ -1,12 +1,10 @@
 package week4;
 
 
-import week3.Sam;
-
 import java.sql.*;
 import java.util.Scanner;
 
-public class JDBCDemo4 {
+public class JDBCDemo5 {
     private static final String url = "jdbc:mysql://127.0.0.1:3306/programmers";
     private static final String username = "root";
     private static final String password = "zikans@19";
@@ -22,30 +20,34 @@ public class JDBCDemo4 {
 
         try {
             Connection connection = DriverManager.getConnection(url, username, password);
-            Statement statement = connection.createStatement();
+            String query = String.format("INSERT INTO Engineers (name, age, marks) VALUES (?, ?, ?)");
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+//            Statement statement = connection.createStatement();
             Scanner scanner = new Scanner(System.in);
             while (true){
                 System.out.print("Enter name: ");
-                String name =scanner.next();
+                String name = scanner.next();
                 System.out.print("Enter age: ");
                 int age = scanner.nextInt();
                 System.out.print("Enter Marks: ");
                 double marks = scanner.nextDouble();
                 System.out.println("Enter more data (Y/N)");
                 String choice = scanner.next();
-                String query = String.format("INSERT INTO Engineers (name, age, marks) VALUES (?, ?, ?)");
-               statement.addBatch(query);
+                preparedStatement.setString(1, name);
+                preparedStatement.setInt(2, age);
+                preparedStatement.setDouble(3, marks);
+                preparedStatement .addBatch();
 
                 if (choice.toUpperCase().equals("N")){
                     break;
                 }
             }
-            int[] arr = statement.executeBatch();
+            int[] arr = preparedStatement.executeBatch();
             for (int i = 0; i <arr.length ; i++) {
                 if(arr[i] == 0){
                     System.out.println("Query: "+i+ "Not successfully executed" );
 
-            }
+                }
             }
 
         } catch (SQLException e) {
