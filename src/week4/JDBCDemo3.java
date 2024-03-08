749cf2b4784cac0fc6573fa553db079015db8ccc
Isaac Sunday Arinze <sunnyanas6@gmail.com>
2024-03-08 15:45:22 +0100

    insert to existing database

--- src/week4/JDBCDemo3.java
+++ src/week4/JDBCDemo3.java
@@ -3,7 +3,7 @@ package week4;
 
 import java.sql.*;
 
-public class JDBCDemo3{
+public class JDBCDemo3 {
 
     //    Create a connection string
     private static final String url = "jdbc:mysql://127.0.0.1:3306/programmers";
@@ -13,29 +13,38 @@ public class JDBCDemo3{
     public static void main(String[] args) {
 
         try {
-            Class.forName(  "com.mysql.cj.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
 
         } catch (ClassNotFoundException exception) {
             System.out.println(exception.getMessage());
 
 //            exception.printStackTrace();
-        };
+        }
+        ;
         try {
             Connection connection = DriverManager.getConnection(url, username, password);
-            Statement statement = connection.createStatement();
+            String query = "INSERT INTO Engineers (name, age, marks) VALUES (?, ?, ?)"; // ADDING A Placeholder
+//            Statement statement = connection.createStatement();
 //            String query = String.format("UPDATE Engineers SET marks = %s WHERE id = %d", 100,  8); update command
-            String query = String.format("DELETE Engineers WHERE id = 2" )
             //String query = "Select * from Engineers";  - select table from database
-            int rowsAffected = statement.executeUpdate(query);
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, "Johnbull");
+            preparedStatement.setInt(2, 25);
+            preparedStatement.setDouble(3, 89.96);
 
-            if (rowsAffected>0){
-                System.out.println("Data successfully deleted");
+            preparedStatement.setString(1, "james");
+            preparedStatement.setInt(2, 12);
+            preparedStatement.setDouble(3, 49.98);
+
+
+            int rowsAffected = preparedStatement.executeUpdate();
+            if (rowsAffected > 0) {
+                System.out.println("Data inserted succefully");
 //                System.out.println("data updated successfully");
-            }else {
-                System.out.println("Data Not Deleted");
-            } 
-
-        }catch (SQLException e) {
+            } else {
+                System.out.println("Data Not Inserted successfully");
+            }
+        } catch (SQLException e) {
 
         }
 
