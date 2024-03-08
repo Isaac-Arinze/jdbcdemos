1f2233d33f4498bea5b0a10eef76f9a1a1c762e2
Isaac Sunday Arinze <sunnyanas6@gmail.com>
2024-03-08 10:51:33 +0100

    database connected

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,11 @@
+
+Q
+!src/week4/UncheckedException.java,0/7/0747c0815a33fcb040457c418ae6b5b93fd767f8
+O
+src/week4/CheckedException.java,8/0/800b776f5932a0c89bdc3fa10737f71434130d6c
+P
+ src/week4/ReadFileException.java,2/3/239426ff8e949c66b80ff01f0b2d811b6ba08bea
+J
+src/week4/WriteToFile.java,5/2/52473f746254b79c4063d2f5f7c136567593b7ed
+L
+src/week4/ExceptionDemo.java,2/e/2e3911d8ec55218e5cab58b8a7adb9f39d67f7ba
--- src/week4/JDBCDemo.java
+++ src/week4/JDBCDemo.java
@@ -5,25 +5,24 @@ import java.sql.*;
 public class JDBCDemo{
 
 //    Create a connection string
-    private static final String url = "jdbc:mysql://127.0.0.1:3306/mydb";
+    private static final String url = "jdbc:mysql://127.0.0.1:3306/programmers";
     private static final String username = "root";
-    private static final String password = "password";
+    private static final String password = "zikans@19";
 
     public static void main(String[] args) {
 
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            Class.forName(  "com.mysql.cj.jdbc.Driver");
 
         } catch (ClassNotFoundException exception) {
-            System.out.println("This is an exception caught in the process" + exception.getMessage());
+            System.out.println(exception.getMessage());
 
 //            exception.printStackTrace();
         };
-
         try {
             Connection connection = DriverManager.getConnection(url, username, password);
             Statement statement = connection.createStatement();
-            String query = "Select * from students";
+            String query = "Select * from Engineers";
             ResultSet resultSet = statement.executeQuery(query);
 
             while (resultSet.next()){
@@ -34,8 +33,7 @@ public class JDBCDemo{
                 System.out.println("ID: " + id);
                 System.out.println("NAME " + name);
                 System.out.println("AGE: " + age);
-                System.out.println("marks " + marks);
-
+                System.out.println("marks " + marks);   
             }
 
         }catch (SQLException e) {
